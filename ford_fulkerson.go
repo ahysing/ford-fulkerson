0097816587603751bed6146d5cf6e47e3fc627d8
Andreas Dreyer Hysing <andreashysing@gmail.com>
2018-03-25 12:57:20 +0200

    Ford fulkerson implemented with helper func main() to se a visual result.

--- ford_fulkerson.go
+++ ford_fulkerson.go
@@ -3,48 +3,60 @@ package main
 import (
 	"errors"
 	"log"
-	"os"
 )
+
+// Converted to GO from from http://www.cse.unt.edu/~tarau/teaching/AnAlgo/Ford–Fulkerson%20algorithm.pdf
 
 // Edge from vertex to vertex with a cost.
 type Edge struct {
-	u        string
-	v        string
+	source   string
+	sink     string
 	capacity float32
 	redge    *Edge
 }
 
 // Graph of vertecies with edges between them.
 type Graph struct {
-	adj  map[Edge]Edge
-	flow map[Edge]float32
+	adjacent map[string][]Edge
+	flow     map[Edge]float32
 }
 
-// Tuple is path and its residual flow
-type Tuple struct {
+// ConnectedEdge is path and its residual flow
+type ConnectedEdge struct {
 	edge     Edge
 	residual float32
 }
 
-// New creates an empty graph
+// New creates an empty Graph
+// Graph is the top level data structure in Ford-Fulkerson
+// A graph is a set of vertecies with edges between them.
+// Edges are stored in `adjacent` while vertecies are defined as properties in every edge
+//
+// The graph also has a flow which states now much recidual capacity is allocated through that
+// peticular edge.
+// After Ford-Fulkerson has run over the all available capacity is allocated to edges.
 func New() Graph {
 	var g Graph
-	g.adj = make(map[Edge]Edge)
+	g.adjacent = make(map[string][]Edge)
 	g.flow = make(map[Edge]float32)
 	return g
 }
 
-func (g Graph) addEdge(u string, v string, capacity float32) error {
+// AddEdge adds an edge to the graph from vertex to vertex.
+// An edge is defined with to vertecies and a capacity between them
+func (g Graph) AddEdge(u string, v string, capacity float32) error {
 	if u == v {
 		return errors.New("U == V")
 	}
 
 	edge := Edge{u, v, capacity, nil}
-	redge := Edge{v, u, 0.0, &edge}
-	edge.redge = &edge
+	redge := Edge{v, u, 0.0, nil}
 
-	g.adj[edge] = redge
-	g.adj[redge] = edge
+	redge.redge = &edge
+	edge.redge = &redge
+
+	g.adjacent[u] = append(g.adjacent[u], edge)
+	g.adjacent[v] = append(g.adjacent[v], redge)
 
 	g.flow[edge] = 0.0
 	g.flow[redge] = 0.0
@@ -52,24 +64,32 @@ func (g Graph) addEdge(u string, v string, capacity float32) error {
 	return nil
 }
 
-func (g Graph) findPath(source string, sink string, path []Tuple, pathSet map[Tuple]bool) []Tuple {
+// AddVertex adds a named node in the directed graph of vertecies
+// A vertex is described by its unique name
+func (g Graph) AddVertex(v string) {
+	g.adjacent[v] = make([]Edge, 1)
+}
+func (g Graph) getEdges(v string) []Edge {
+	return g.adjacent[v]
+}
+
+func (g Graph) findPath(source string, sink string, path []ConnectedEdge, pathSet map[ConnectedEdge]bool) []ConnectedEdge {
 	if source == sink {
 		return path
 	}
 
-	for edge := range g.getEdges(source) {
+	edges := g.getEdges(source)
+	for _, edge := range edges {
 		residual := edge.capacity - g.flow[edge]
-		tuple := Tuple{
-			source,
-			residual
-		}
+		connectedEdge := ConnectedEdge{edge, residual}
 
-		_, hasTuple := pathSet
-		if residual > 0 && !hasTuple {
-			pathSet[Tuple] = true
-			result := g.findPath(edge.sink, sink, pathSet)
-			if result != nil {
-				return result
+		_, hasConnectedEdge := pathSet[connectedEdge]
+		if residual > 0.0 && !hasConnectedEdge {
+			pathSet[connectedEdge] = true
+			nextPath := append(path, connectedEdge)
+			finalPath := g.findPath(edge.sink, sink, nextPath, pathSet)
+			if finalPath != nil && len(finalPath) > 0 { // TODO: light checking
+				return finalPath
 			}
 		}
 	}
@@ -77,60 +97,83 @@ func (g Graph) findPath(source string, sink string, path []Tuple, pathSet map[Tu
 	return nil
 }
 
-func (g Graph) maxFlow(source string, sink string) {
-	slice := make([]Tuple)
-	start := make(map[Edge]float32)
-	path := g.findPath(source, sink, slice, start)
+// FordFulkerson traverses the graph according to the algorithm and assigns the maximum flow in the graph.
+// The maximum flow lives inside property `flow`
+func FordFulkerson(g Graph, source string, sink string) Graph {
+	newPath := make([]ConnectedEdge, 0)
+	newPathSet := make(map[ConnectedEdge]bool)
+
+	path := g.findPath(source, sink, newPath, newPathSet)
 	for path != nil {
-		flow := 2147483647.0
-		for _, res := range path {
-			if res < flow {
-				flow = res
+		flow := float32(2147483647.0)
+		for _, p := range path {
+			if p.residual > 0.0 && p.residual < flow {
+				flow = p.residual
 			}
 		}
 
-		for edge, res := range path {
-			g.flow[edge] += flow
-			g.flow[edge.redge] -= flow
+		for _, p := range path {
+			edge := p.edge
+
+			g.flow[edge] = g.flow[edge] + flow
+			g.flow[*edge.redge] -= flow
+			if edge == *edge.redge {
+				panic("edge == reverse edge")
+			}
 		}
 
-		slice = make([]Tuple)
-		start = make(map[Edge]float32)
-		path = g.findPath(source, sink, slice, start)
+		newPath = make([]ConnectedEdge, 0)
+		newPathSet = make(map[ConnectedEdge]bool)
+		path = g.findPath(source, sink, newPath, newPathSet)
 	}
 
-	var sum float32
-	for edge := range g.getEdges(source) {
-		sum += g.flow[edge]
-	}
-
-	return sum
+	return g
 }
 
-func fordFulkerson() {
-	var graph = New()
-	verts := []string{"s", "o", "p", "q", "r", "t"}
-	for vertex := range verts {
-		g.addVertex(vertex)
+// MaxFlow finds the maximum flow through a graph where Ford-Fulkerson has assigned the maximum flow
+func (g Graph) MaxFlow(source string, sink string) float32 {
+	var maxFlow float32
+
+	for _, edge := range g.getEdges(source) {
+		maxFlow += g.flow[edge]
 	}
 
-	g.addEdge("s", "o", 3)
-	g.addEdge("s", "p", 3)
-	g.addEdge("o", "p", 2)
-	g.addEdge("o", "q", 3)
-	g.addEdge("p", "r", 2)
-	g.addEdge("r", "t", 3)
-	g.addEdge("q", "r", 4)
-	g.addEdge("q", "t", 2)
+	return maxFlow
+}
 
-	result := g.maxFlow("s", "t")
-	log.Println(result)
+func buildExampleGraph() Graph {
+	var g = New()
+	verts := []string{"s", "o", "p", "q", "r", "t"}
+	for _, vertex := range verts {
+		g.AddVertex(vertex)
+	}
+
+	g.AddEdge("s", "o", 3)
+	g.AddEdge("s", "p", 3)
+	g.AddEdge("o", "p", 2)
+	g.AddEdge("o", "q", 3)
+	g.AddEdge("p", "r", 2)
+	g.AddEdge("r", "t", 3)
+	g.AddEdge("q", "r", 4)
+	g.AddEdge("q", "t", 2)
+	return g
 }
 
 func main() {
-	if len(os.Args) >= 2 {
-		log.Printf("Error")
-	} else {
-		fordFulkerson()
+	g := buildExampleGraph()
+	graphTraversed := FordFulkerson(g, "s", "t")
+	result := graphTraversed.MaxFlow("s", "t")
+
+	log.Println("== Printing example graph ==")
+	for vertex, edges := range g.adjacent {
+		log.Println("Vertex ", vertex)
+		for _, edge := range edges {
+			if edge.capacity > 0 {
+				log.Printf("Connected to %v with capcaity %v", edge.sink, edge.capacity)
+			}
+		}
 	}
+	log.Println(" == Graph finished ==")
+
+	log.Printf("Flow from s to t is %v", result)
 }
